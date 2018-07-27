1f5bc4d378d49bcc54c6f56cf8b0aa621e263ff3
Yang <yfu@bazean.com>
2018-07-27 15:12:39 -0700

    delete private code

--- baz/loading_utils/sql_scripts/create_new_files.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS sandbox.new_files
-(
-   file_location  text,
-   destination_table text,
-   destination_schema text,
-   plan_load_date	  timestamp,
-   date_created DATE
-);
